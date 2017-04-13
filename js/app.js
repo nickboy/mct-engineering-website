ac65b7a0630b10c05094034dfccaa032f0ed75c8
Nick Liu <nickliu0211@gmail.com>
2017-04-13 12:22:11 -0500

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,34 @@
+var items = [
+  {
+    "id": 'ON001',
+    "name": 'Concrete Foundation'
+  },
+  {
+    "id": 'ON002',
+    "name": 'Concrete Foundation'
+  },
+  {
+    "id": 'ON003',
+    "name": 'Material Take-off',
+  },
+  {
+    "id": 'OS001',
+    "name": 'DSU'
+  },
+  {
+    "id": 'OS001',
+    "name": 'Concrete Foundation'
+  },
+  {
+    "id": 'OS001',
+    "name": 'Concrete Foundation'
+  },
+]
+
+var app2 = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue!'
+    data: 
+  }
+})
