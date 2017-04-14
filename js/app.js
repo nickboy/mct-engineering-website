7a4a9f0063599b4b2054d21a1d3d56a933a6e1bd
Nick Liu <nickliu0211@gmail.com>
2017-04-14 14:56:13 -0500

    Add Vue.js support for quatation section.

--- js/app.js
+++ js/app.js
@@ -1,34 +1,55 @@
 var items = [
   {
-    "id": 'ON001',
-    "name": 'Concrete Foundation'
+    "id": "ON001",
+    "type": "Onshore",
+    "name": "Concrete Foundation"
   },
   {
-    "id": 'ON002',
-    "name": 'Concrete Foundation'
+    "id": "ON002",
+    "type": "Onshore",
+    "name": "Pipe Rack"
   },
   {
-    "id": 'ON003',
-    "name": 'Material Take-off',
+    "id": "ON003",
+    "type": "Onshore",
+    "name": "Material Take-off"
   },
   {
-    "id": 'OS001',
-    "name": 'DSU'
+    "id": "OS001",
+    "type": "Offshore",
+    "name": "DSU"
   },
   {
-    "id": 'OS001',
-    "name": 'Concrete Foundation'
+    "id": "OS002",
+    "type": "Offshore",
+    "name": "TBM"
   },
   {
-    "id": 'OS001',
-    "name": 'Concrete Foundation'
+    "id": "OS003",
+    "type": "Offshore",
+    "name": "CRANE BOOM REST"
   },
+  {
+    "id": "SS001",
+    "type": "Subsea Structural Geotechnical",
+    "name": "PLET"
+  },
+  {
+    "id": "SS002",
+    "type": "Subsea Structural Geotechnical",
+    "name": "SUCTION PILE"
+  },
+  {
+    "id": "SS003",
+    "type": "Subsea Structural Geotechnical",
+    "name": "CATHODIC PROTECTIO"
+  }
 ]
 
-var app2 = new Vue({
+var app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue!'
-    data: 
+    message: 'Hello Vue!',
+    items: items
   }
 })
